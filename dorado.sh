15faad275b921d15ab7ece5f0c7c4b74ef024d4d
Max Emil Schoen <9975295+maxemil@users.noreply.github.com>
2024-01-19 09:48:52 +0100

    update nanopore models

--- dorado.sh
+++ dorado.sh
@@ -71,7 +71,7 @@ echo "Cell:   $cell"
 
 # select model
 declare -A models=(
-    ["FLO-MIN106"]="dna_r9.4.1_e8_sup@v3.3"
+    ["FLO-MIN106"]="dna_r9.4.1_e8_sup@v3.6"
     ["FLO-MIN111"]="dna_r10.3@v3.3"
     ["FLO-MIN112"]="dna_r10.4_e8.1_sup@v3.4"
     ["FLO-MIN114"]="dna_r10.4.1_e8.2_400bps_sup@v4.3.0")
